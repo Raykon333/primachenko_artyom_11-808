55df49028d987727c658dcd78886dcf66e362c75
Raykon <primachenko.artem@mail.ru>
2018-09-16 14:12:11 +0400

    First commit test

---
+++ 2018/Fall/Misc/String.Format/String.Format/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(GetGreetingMessage("Student", 10.01));
+            Console.WriteLine(GetGreetingMessage("Bill Gates", 10000000.5));
+            Console.WriteLine(GetGreetingMessage("Steve Jobs", 1));
+        }
+
+        private static string GetGreetingMessage(string name, double salary)
+        {
+            // возвращает "Hello, <name>, your salary is <salary>"
+            return String.Format("Hello, {0}, your salary is {1}", name, Math.Ceiling(salary));
+        }
+    }
+}
