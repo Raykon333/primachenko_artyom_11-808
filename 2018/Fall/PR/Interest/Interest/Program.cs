7014e15811079b07a52920c1bd9b01e695c54b7f
Raykon <primachenko.artem@mail.ru>
2018-09-16 14:42:22 +0400

    PR-Interest

---
+++ 2018/Fall/PR/Interest/Interest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = double.Parse(Console.ReadLine());
+            double interest = double.Parse(Console.ReadLine());
+            int term = int.Parse(Console.ReadLine());
+            Console.WriteLine(Calculate(sum, interest, term).ToString());
+        }
+        public static double Calculate(double sum, double interest, int term)
+        {
+            sum = sum * Math.Pow(((1 + interest / 12 * 0.01)), term);
+            return sum;
+        }
+    }
+}
