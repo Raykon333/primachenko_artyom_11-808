baa515fe8f72d2bbad518f45920561018ab20a34
1 <primachenko.artem@mail.ru>
2019-02-22 11:36:58 +0300

    Farey&Bonds&YR1

---
+++ 2019/Spring/AiSD/Bonds/Bonds/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Bonds
+{
+
+    class TreeItem
+    {
+        public int Next;
+        public int Weight = 1;
+
+        public TreeItem(int value)
+        {
+            Next = value;
+        }
+    }
+
+    static class Bonds
+    {
+        public static TreeItem[] treeFastest;
+        public static TreeItem[] treeFast;
+
+        static Bonds()
+        {
+            int size = 100000;
+            treeFastest = new TreeItem[size];
+            treeFast = new TreeItem[size];
+            for (int i = 0; i < size; i++)
+            {
+                treeFastest[i] = new TreeItem(i);
+                treeFast[i] = new TreeItem(i);
+            }
+        }
+
+        public static string ConnectFastest(int point1, int point2)
+        {
+            point1 = FindStarostaFastest(point1);
+            point2 = FindStarostaFastest(point2);
+            if (point1 == point2)
+                return "Failed";
+            if (treeFastest[point1].Weight > treeFastest[point2].Weight)
+            {
+                treeFastest[point2].Next = point1;
+                treeFastest[point2].Weight++;
+            }
+            else
+            {
+                treeFastest[point1].Next = point2;
+                treeFastest[point1].Weight++;
+            }
+            return "Succeeded";
+        }
+
+        public static string ConnectFast(int point1, int point2)
+        {
+            point1 = FindStarostaFast(point1);
+            point2 = FindStarostaFast(point2);
+            if (point1 == point2)
+                return "Failed";
+            if (treeFast[point1].Weight > treeFast[point2].Weight)
+            {
+                treeFast[point2].Next = point1;
+                treeFast[point2].Weight++;
+            }
+            else
+            {
+                treeFast[point1].Next = point2;
+                treeFast[point1].Weight++;
+            }
+            return "Succeeded";
+        }
+
+        public static int FindStarostaFastest(int point)
+        {
+            while (treeFastest[point].Next != point)
+            {
+                int buffer = point;
+                point = treeFastest[point].Next;
+                treeFastest[buffer].Next = treeFastest[point].Next;
+            }
+            return point;
+        }
+        
+        public static int FindStarostaFast(int point)
+        {
+            while (treeFast[point].Next != point)
+            {
+                point = treeFast[point].Next;
+            }
+            return point;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            for(int i = 1; i <= 99999; i++)
+            {
+                stopwatch.Restart();
+                Bonds.ConnectFast(0, i);
+                long fast = stopwatch.ElapsedTicks;
+                stopwatch.Restart();
+                Bonds.ConnectFastest(0, i);
+                long fastest = stopwatch.ElapsedTicks;
+                stopwatch.Stop();
+                Console.WriteLine("Fast: " + fast + "\tFastest: " + fastest);
+            }
+        }
+    }
+}
---
+++ 2019/Spring/SEM/YR1/YR1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YR1
+{
+    interface IFormula
+    {
+        int Size { get; }
+        string Write();
+    }
+
+    class Constant : IFormula
+    {
+        public char constant;
+        public int Size { get { return 0; } }
+
+        public string Write()
+        {
+            return constant.ToString();
+        }
+    }
+
+    class Formula : IFormula
+    {
+        public IFormula Formula1;
+        public IFormula Formula2;
+        public char Operation;
+        public bool IsBracketed;
+        public int Size { get { return Formula1.Size + Formula2.Size + 1; } }
+
+        public string Write()
+        {
+            if (IsBracketed)
+                return '(' + Formula1.Write() + Operation + Formula2.Write() + ')';
+            return Formula1.Write() + Operation + Formula2.Write();
+        }
+    }
+
+    class Program
+    {
+        private static char[] constants = new char[] { '0', '1', '2', 'x' };
+        private static char[] operations = new char[] { '+', '-', '*' };
+        private static bool[] bools = new bool[] { false, true };
+
+        static void Main(string[] args)
+        {
+            foreach (var x in GetAllOfSize(2))
+                Console.WriteLine(x.Write());
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<IFormula> GetAllOfSize(int size)
+        {
+            if (size == 0)
+                foreach (var constant in constants)
+                    yield return new Constant { constant = constant };
+            else
+                foreach(var formula1 in GetAllOfSize(size - 1))
+                    foreach(var formula2 in GetAllOfSize(0))
+                        foreach(var operation in operations)
+                            foreach(var boolx in bools)
+                            {
+                                Formula result = new Formula() { Formula1 = formula1, Formula2 = formula2, Operation = operation, IsBracketed = boolx };
+                                yield return result;
+                            }
+        }
+    }
+}
