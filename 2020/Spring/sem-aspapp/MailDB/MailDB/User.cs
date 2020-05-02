6bcc9e1c1c97be84fd4d354e1cdecfab754c77da
senpaiquacker <42979404+senpaiquacker@users.noreply.github.com>
2020-05-02 12:55:40 +0300

    First Commit
    Пример того, как может быть устроена база данных (если что, если сервер работает внутри, он реально может получать доступ к своим дисковым носителям, а потому можно будет скорее всего спокойно юзать Directory, DirectoryInfo и так далее. По поводу класса письма чуть позже пришлю, изменю старый код и добавлю новый

---
+++ 2020/Spring/sem-aspapp/MailDB/MailDB/User.cs
@@ -0,0 +1,11 @@
+﻿using MailWorker;
+
+namespace MailDB
+{
+    public class User
+    {
+        public string Address;
+        public string Password;
+        public FileWorker Mail = new FileWorker(@"D:\Mail");
+    }
+}
---
+++ 2020/Spring/sem-aspapp/MailWorker/MailWorker/FileWorker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace MailWorker
+{
+    public class FileWorker
+    {
+        public string FileDirectory {get;}
+        public FileWorker(string directory)
+        {
+            FileDirectory = directory;
+        }
+
+        public string[] GetIncomingMessages(string address)
+        {
+            string incomingDir = FileDirectory + @"\" + address + @"\Incoming";
+            return Directory
+                .GetFiles(incomingDir)
+                .Select(file => File.ReadAllText(incomingDir + @"\" + file))
+                .ToArray();
+        }
+
+        public string[] GetOutcomingMessages(string address)
+        {
+            string outcomingDir = FileDirectory + @"\" + address + @"\Outcoming";
+            return Directory
+                .GetFiles(outcomingDir)
+                .Select(file => File.ReadAllText(outcomingDir + @"\" + file))
+                .ToArray();
+        }
+
+        /*public string [] GetSpamMessages(string address)
+        {
+            string spamDir = FileDirectory + @"\" + address + @"\Spam";
+            return Directory
+                .GetFiles(spamDir)
+                .Select(file => File.ReadAllText(spamDir + @"\" + file))
+                .ToArray();
+        }*/
+    }
+}
